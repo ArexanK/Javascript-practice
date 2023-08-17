167b646281415c60d24408e8220404fe657ec206
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2023-08-17 12:53:23 +0200

    arrays practice

---
+++ Arrays/script.js
@@ -0,0 +1,18 @@
+// create a function that takes in an array. i
+//f the first number, is less thab the last number alert 'hi. 
+//if the first number is greater than the last number aler 'bye'.
+// if they are equal, alert 'we close in an hour'
+
+function compareArray(arr){
+if(arr[0] < arr [arr.length -1 ]){
+alert('hi')
+} else if ( arr[0] > arr[arr.length-1]){
+    alert('Bye')
+} else {
+    aler('we are closed')
+
+}
+}
+
+compareArray([5,2,3,4,1])
+compareArray([5,2,3,4,5])
